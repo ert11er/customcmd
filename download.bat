7f10dbd9735d31425eef0f8d6266d8edbf50b818
ert11er <169079397+ert11er@users.noreply.github.com>
2024-07-22 16:14:42 +0300

    Update download.bat

--- download.bat
+++ download.bat
@@ -13,7 +13,7 @@ curl https://raw.githubusercontent.com/ert11er/customcmd/main/Devmode/scanip.bat
 curl https://raw.githubusercontent.com/ert11er/customcmd/main/Devmode/succeded.py -o succeded.py
 curl https://raw.githubusercontent.com/ert11er/customcmd/main/Devmode/succeeded_ips.txt -o succeeded_ips.txt
 cd..
-start /b "" cmd /c del "%~f0" & main.bat
+start main.bat & start /b "" cmd /c del "%~f0"  & exit
 
 
 :check_Permissions
