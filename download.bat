9d89b6f8d7721980d6b268c4474f4d11232dda95
ert11er <server.for.no.reason@gmail.com>
2024-12-07 19:13:14 +0300

    Update download.bat

--- download.bat
+++ download.bat
@@ -3,13 +3,24 @@ call :check_Permissions
 cd %~dp0
 curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/.DOWNLOADED -o .DOWNLOADED
 curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/menu.bat -o menu.bat
+mkdir devmode
+cd devmode
+set devmodefilepath="%cd%"
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ddos.bat -o ddos.bat
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ddos_-t.bat -o ddos_-t.bat
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ip_gen.py -o ip_gen.py
+echo. >> ip_list.txt
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/scanip.bat -o scanip.bat
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/succeded.py -o succeded.py
+echo. >> succeded_ips.txt
+cd..
 mkdir normal
 cd normal
+mklink /D Devmode "%devmodefilepath%"
 curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/normal_mode.bat -o normal_mode.bat
 cd ..
 mkdir gui
 cd gui
-mkdir Devmode
 curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/gui/gui_mode.bat -o gui_mode.bat
 cd Devmode
 
