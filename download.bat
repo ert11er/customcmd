cbd689a5c446da6809b5ae071606864548f21908
ert11er <server.for.no.reason@gmail.com>
2024-12-07 18:46:23 +0300

    Update download.bat

--- download.bat
+++ download.bat
@@ -4,7 +4,18 @@ cd %~dp0
 mkdir normal
 cd normal
 mkdir Devmode
-
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/normal_mode.bat -o normal_mode.bat
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/.DOWNLOADED -o normal_mode.bat
+cd Devmode
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/Devmode/ddos.bat -o ddos.bat
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/Devmode/ddos_-t.bat -o ddos_-t.bat
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/Devmode/ip_gen.py -o ip_gen.py
+echo. >> ip_list.txt
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/Devmode/scanip.bat -o scanip.bat
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/Devmode/succeded.py -o succeded.py
+echo. >> succeded_ips.txt
+cd ..
+cd ..
 
 :check_Permissions
 net session >nul 2>&1
