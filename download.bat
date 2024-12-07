049e439cc84422e87bcfebd9cf9ee1b3b0502736
ert11er <server.for.no.reason@gmail.com>
2024-12-07 19:23:55 +0300

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,40 +1,35 @@
 @echo off
-call :check_Permissions
-cd %~dp0
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/.DOWNLOADED -o .DOWNLOADED
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/menu.bat -o menu.bat
-mkdir devmode
-cd devmode
-set devmodefilepath="%cd%"
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ddos.bat -o ddos.bat
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ddos_-t.bat -o ddos_-t.bat
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ip_gen.py -o ip_gen.py
-echo. >> ip_list.txt
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/scanip.bat -o scanip.bat
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/succeded.py -o succeded.py
-echo. >> succeded_ips.txt
-cd..
-mkdir normal
-cd normal
+call :check_Permissions & cd /d "%~dp0"
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/.DOWNLOADED -o .DOWNLOADED
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/menu.bat -o menu.bat
+mkdir devmode & cd /d devmode
+set "devmodefilepath=%cd%"
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ddos.bat -o ddos.bat
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ddos_-t.bat -o ddos_-t.bat
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ip_gen.py -o ip_gen.py
+echo. > ip_list.txt
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/scanip.bat -o scanip.bat
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/succeded.py -o succeded.py
+echo. > succeded_ips.txt
+cd /d .. & mkdir normal & cd /d normal
 mklink /D Devmode "%devmodefilepath%"
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/normal_mode.bat -o normal_mode.bat
-cd ..
-mkdir gui
-cd gui
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/gui/gui_mode.bat -o gui_mode.bat
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/normal_mode.bat -o normal_mode.bat
+cd /d .. & mkdir gui & cd /d gui
+curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/gui/gui_mode.bat -o gui_mode.bat
 mklink /D Devmode "%devmodefilepath%"
-cd..
-mkdir NOUI
-cd NOUI
-mkdir customcmd-cli
-cd customcmd-cli
+cd /d .. & mkdir NOUI & cd /d NOUI & mkdir customcmd-cli & cd /d customcmd-cli
 
 
 :check_Permissions
-net session >nul 2>&1
-if %errorLevel% == 0 (
+net session >nul 2>&1 & if %errorlevel% == 0 (
     cls
 ) else (
-    powershell.exe -Command "Start-Process -Verb RunAs -FilePath \"%~f0\""
-    exit
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%~f0'"
+    exit /b
 )
+
+
+
+
+
+
