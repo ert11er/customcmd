d3a2053399f5426bcdf618d2fac7f6851ae0cf6d
ert11er <server.for.no.reason@gmail.com>
2024-12-07 19:31:40 +0300

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,23 +1,37 @@
 @echo off
-call :check_Permissions & cd /d "%~dp0"
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/.DOWNLOADED -o .DOWNLOADED
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/menu.bat -o menu.bat
-mkdir devmode & cd /d devmode
+call :check_Permissions
+cd /d "%~dp0"
+
+IF EXIST ".DOWNLOADED" (
+    echo Files already downloaded. Skipping download...
+    goto :eof
+)
+
+:: Download main files
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/.DOWNLOADED" -o ".DOWNLOADED" >nul 2>&1
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/menu.bat" -o "menu.bat" >nul 2>&1
+
+:: Create Devmode directory and download files
+mkdir Devmode
+pushd Devmode
 set "devmodefilepath=%cd%"
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ddos.bat -o ddos.bat
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ddos_-t.bat -o ddos_-t.bat
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/ip_gen.py -o ip_gen.py
-echo. > ip_list.txt
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/scanip.bat -o scanip.bat
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/succeded.py -o succeded.py
-echo. > succeded_ips.txt
-cd /d .. & mkdir normal & cd /d normal
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/*" -o @ - >nul 2>&1
+echo "" > ip_list.txt 
+
+echo "" > succeeded_ips.txt
+cd /d ..
+mkdir Normal & cd /d Normal
 mklink /D Devmode "%devmodefilepath%"
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/normal_mode.bat -o normal_mode.bat
-cd /d .. & mkdir gui & cd /d gui
-curl -L https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/gui/gui_mode.bat -o gui_mode.bat
-mklink /D Devmode "%devmodefilepath%"
-cd /d .. & mkdir NOUI & cd /d NOUI & mkdir customcmd-cli & cd /d customcmd-cli
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Normal/normal_mode.bat" -o "normal_mode.bat" >nul 2>&1
+cd /d ..
+mkdir GUI & cd /d GUI
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/GUI/gui.bat" -o "gui.bat" >nul 2>&1
+cd /d ..
+mkdir NOUI & cd /d NOUI
+mkdir customcmd-cli & cd /d customcmd-cli
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/NOUI/noui.bat" -o "noui.bat" >nul 2>&1
+
+
 
 
 :check_Permissions
