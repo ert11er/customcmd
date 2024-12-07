d690e3686e489d4052070b7403f7e23fa59ed09b
ert11er <server.for.no.reason@gmail.com>
2024-12-07 19:06:06 +0300

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,7 +1,7 @@
 @echo off
 call :check_Permissions
 cd %~dp0
-curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/normal/.DOWNLOADED -o .DOWNLOADED
+curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/.DOWNLOADED -o .DOWNLOADED
 curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/menu.bat -o menu.bat
 mkdir normal
 cd normal
@@ -22,6 +22,7 @@ cd gui
 mkdir Devmode
 curl https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/gui/gui_mode.bat -o gui_mode.bat
 cd Devmode
+
 
 
 :check_Permissions
