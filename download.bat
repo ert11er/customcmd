77346fa695e299be04019bf4ecc1f9b1c9a6fc2f
ert11er <server.for.no.reason@gmail.com>
2024-12-07 19:35:26 +0300

    Update download.bat

--- download.bat
+++ download.bat
@@ -7,31 +7,38 @@ IF EXIST ".DOWNLOADED" (
     goto :eof
 )
 
-:: Download main files
-curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/.DOWNLOADED" -o ".DOWNLOADED" >nul 2>&1
-curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/menu.bat" -o "menu.bat" >nul 2>&1
+:: Download .DOWNLOADED file
+echo Downloading .DOWNLOADED...
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/.DOWNLOADED" -o ".DOWNLOADED"
 
-:: Create Devmode directory and download files
+:: Download menu.bat
+echo Downloading menu.bat...
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/menu.bat" -o "menu.bat"
+
+:: Create Devmode directory and download Devmode files
 mkdir Devmode
 pushd Devmode
 set "devmodefilepath=%cd%"
-curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/*" -o @ - >nul 2>&1
-echo "" > ip_list.txt 
-
+echo Downloading Devmode files...
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Devmode/*" -o @ -
+echo "" > ip_list.txt
 echo "" > succeeded_ips.txt
 cd /d ..
 mkdir Normal & cd /d Normal
 mklink /D Devmode "%devmodefilepath%"
-curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Normal/normal_mode.bat" -o "normal_mode.bat" >nul 2>&1
+:: Download normal_mode.bat
+echo Downloading normal_mode.bat...
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/Normal/normal_mode.bat" -o "normal_mode.bat"
 cd /d ..
 mkdir GUI & cd /d GUI
-curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/GUI/gui.bat" -o "gui.bat" >nul 2>&1
+:: Download gui.bat
+echo Downloading gui.bat...
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/GUI/gui.bat" -o "gui.bat"
 cd /d ..
 mkdir NOUI & cd /d NOUI
 mkdir customcmd-cli & cd /d customcmd-cli
-curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/NOUI/noui.bat" -o "noui.bat" >nul 2>&1
-
-
+echo Downloading noui.bat...
+curl -L "https://raw.githubusercontent.com/ert11er/customcmd/refs/heads/main/NOUI/customcmd-cli/customcmd-cli.bat" -o "noui.bat"
 
 
 :check_Permissions
