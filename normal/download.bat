c817147deaf17eb4462f9203ab39ac87129f273c
ert11er <server.for.no.reason@gmail.com>
2024-12-07 18:35:44 +0300

    Update download.bat

--- normal/download.bat
+++ normal/download.bat
@@ -1,2 +1,18 @@
 @echo off
+call :check_Permissions
+cd %~dp0
 
+
+
+
+
+
+
+:check_Permissions
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    cls
+) else (
+    powershell.exe -Command "Start-Process -Verb RunAs -FilePath \"%~f0\""
+    exit
+)
