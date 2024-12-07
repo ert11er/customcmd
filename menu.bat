863dd62049dd7f3f49cf0db20332197efa36c95b
ert11er <server.for.no.reason@gmail.com>
2024-12-07 19:22:25 +0300

    Update menu.bat

--- menu.bat
+++ menu.bat
@@ -1,4 +1,6 @@
 @echo off
+call :check_Permissions
+cd %~dp0
 echo 1. normal mode
 echo 2. gui mode
 set /p input="Select your menu:"
@@ -9,4 +11,16 @@ start normal_mode.bat
 if "%input%" == "2" (
 cd gui
 start gui_mode.bat
+)
+
+
+
+
+:check_Permissions
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    cls
+) else (
+    powershell.exe -Command "Start-Process -Verb RunAs -FilePath \"%~f0\""
+    exit
 )
