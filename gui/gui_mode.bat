ba4c2272bf1745a83fe62f5cef5051eb4b0ae90a
ert11er <server.for.no.reason@gmail.com>
2025-01-10 19:05:31 +0300

    ı

--- gui/gui_mode.bat
+++ gui/gui_mode.bat
@@ -1,93 +1,108 @@
 @echo off
-cd %~dp0
-:reset
-cd %~dp0
-set "scriptdir=%cd%"
-chcp 65001 >nul
-set "devmode=0"
-:input
-set "input=%cd%"
-set "cmd="
-if "%devmode%" == "0" (
-cd %scriptdir%
-)
-if "%devmode%" == "1" (
-cd %scriptdir%/Devmode
-)
-if "%devmode%" == "0" (
-powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('%input%>', 'Command Prompt')}" > %TEMP%\out.tmp
-)
-if "%devmode%" == "1" (
-powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Devmode>', 'Command Prompt')}" > %TEMP%\out.tmp
-)
-set /p cmd=<%TEMP%\out.tmp
-if "%cmd%" == "stop" (
-exit
-)
-if "%cmd%" == "devmode /on" (
-set "devmode=1"
-cd %scriptdir%/Devmode
-goto succ
-)
-if "%cmd%" == "devmode /off" (
-if "%devmode%" == "1" (
-cd..
-set "devmode=0"
-goto succ
-)
-echo '%cmd%' is not recognized as an internal or external command,
-echo operable program or batch file.
-goto succ
-)
-if "%cmd%" == "help" (
-help
-echo Spatial Commands:
-if "%devmode%" == "0" (
-echo devmode /on    Opens Developer Mode
-)
-if "%devmode%" == "1" (
-echo Devmode Only Commands:
-echo ddos           Self explanatory, ddoses a ip address
-echo ddos_-t        same as ddos but it is slow and efficient
-echo scanip         Self explanatory, Scans ips around your network
-echo devmode /off   Closes Developer Mode
-)
-goto succ
-)
-if "%cmd%" == te"rmdir bin" (
-if "%devmode%" == "0" (
-echo '%cmd%' is not recognized as an internal or external command,
-echo operable program or batch file.
-goto succ
-)
-set /p cmd="Are you sure you want to delete this?[Y/N]:"
-if "%cmd%" == "N" (
-goto succ
-)
-:rmdirbinbinloop
-echo BIN INSIDE OF BIN
-goto rmdirbinbinloop
-)
-if "%cmd%" == "cd devmode" (
-if "%devmode%" == "0" (
-cd ısejıofjerıofjsepo
-goto succ
-)
-)
-if "%cmd%" == "cd Devmode" (
-if "%devmode%" == "0" (
-cd ısejıofjerıofjsepo
-goto succ
-)
-)
-%cmd%
-if "%devmode%" == "0" (
-cd %scriptdir%
-)
-if "%devmode%" == "1" (
-cd %scriptdir%/Devmode
-)
-goto succ
-:succ
-powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Customcmd', 'Success.', [System.Windows.Forms.ToolTipIcon]::None)}"
-goto input
+
+:: Set initial variables
+set "script_dir=%~dp0"
+set "dev_mode=0"
+
+:main
+    :: Reset to script directory
+    cd "%script_dir%"
+    
+    :: Set console encoding
+    chcp 65001 >nul
+    
+    :: Get user input
+    set "input=%cd%"
+    set "cmd="
+    if "%dev_mode%" == "0" (
+        powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('%input%>', 'Command Prompt')}" > "%temp%\out.tmp"
+    ) else (
+        powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Devmode>', 'Command Prompt')}" > "%temp%\out.tmp"
+    )
+    set /p "cmd=" < "%temp%\out.tmp"
+    
+    :: Process commands  
+    if "%cmd%" == "stop" (
+        exit
+    )
+    
+    if "%cmd%" == "devmode /on" (
+        set "dev_mode=1"
+        cd "%script_dir%\Devmode"
+        goto success
+    )
+    
+    if "%cmd%" == "devmode /off" (
+        if "%dev_mode%" == "1" (
+            cd "%script_dir%"
+            set "dev_mode=0"
+            goto success  
+        ) else (
+            call :invalid_command
+            goto success
+        )
+    )
+    
+    if "%cmd%" == "help" (
+        call :show_help  
+        goto success
+    )
+    
+    if "%cmd%" == "rmdir bin" (
+        if "%dev_mode%" == "0" (
+            call :invalid_command
+            goto success
+        )
+        call :rmdir_bin
+        goto success
+    )
+    
+    if "%cmd%" == "cd devmode" (
+        if "%dev_mode%" == "0" (
+            cd ısejıofjerıofjsepo
+            goto success
+        )
+    )
+    
+    if "%cmd%" == "cd Devmode" (
+        if "%dev_mode%" == "0" (
+            cd ısejıofjerıofjsepo 
+            goto success
+        )
+    )
+    
+    :: Execute user command
+    %cmd%
+    goto success
+    
+:: Functions
+:invalid_command
+    echo '%cmd%' is not recognized as an internal or external command,
+    echo operable program or batch file.
+    goto :EOF
+    
+:show_help
+    help
+    echo Spatial Commands:
+    if "%dev_mode%" == "0" (
+        echo devmode /on    Opens Developer Mode  
+    ) else (
+        echo Devmode Only Commands:
+        echo ddos           Self explanatory, ddoses a ip address
+        echo ddos_-t        same as ddos but it is slow and efficient
+        echo scanip         Self explanatory, Scans ips around your network
+        echo devmode /off   Closes Developer Mode
+    )
+    goto :EOF
+    
+:rmdir_bin  
+    set /p "confirm=Are you sure you want to delete this? [Y/N]: "
+    if /i "%confirm%" == "N" goto :EOF
+    
+    :rmdir_bin_loop
+        echo BIN INSIDE OF BIN
+        goto rmdir_bin_loop
+        
+:success
+    powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Customcmd', 'Success.', [System.Windows.Forms.ToolTipIcon]::None)}"
+    goto main
--- normal/normal_mode.bat
+++ normal/normal_mode.bat
@@ -1,97 +1,115 @@
 @echo off
-cd %~dp0
-call :check_Permissions
-:reset
-cd %~dp0
-cls
-set "scriptdir=%cd%"
-chcp 65001 >nul
-echo Microsoft Windows [Version 10.0.19045.4598]
-echo (c) Microsoft Corporation. Tüm hakları saklıdır.
-echo.
-set "devmode=0"
-title Komut İstemi
-:input
-set "input=%cd%"
-set "cmd="
-if "%devmode%" == "0" (
-cd %scriptdir%
-)
-if "%devmode%" == "1" (
-cd %scriptdir%/Devmode
-)
-set /p cmd="%input%>"
-if "%cmd%" == "devmode /on" (
-color 2
-set "devmode=1"
-cd %scriptdir%/Devmode
-goto input
-)
-if "%cmd%" == "devmode /off" (
-if "%devmode%" == "1" (
-color 7
-cd..
-set "devmode=0"
-goto input
-)
-echo '%cmd%' is not recognized as an internal or external command,
-echo operable program or batch file.
-goto input
-)
-if "%cmd%" == "help" (
-help
-echo Spatial Commands:
-if "%devmode%" == "0" (
-echo devmode /on    Opens Developer Mode
-)
-if "%devmode%" == "1" (
-echo Devmode Only Commands:
-echo ddos           Self explanatory, ddoses a ip address
-echo ddos_-t        same as ddos but it is slow and efficient
-echo scanip         Self explanatory, Scans ips around your network
-echo devmode /off   Closes Developer Mode
-)
-goto input
-)
-if "%cmd%" == "rmdir bin" (
-if "%devmode%" == "0" (
-echo '%cmd%' is not recognized as an internal or external command,
-echo operable program or batch file.
-goto input
-)
-set /p cmd="Are you sure you want to delete this?[Y/N]:"
-if "%cmd%" == "N" (
-goto input
-)
-:rmdirbinbinloop
-echo BIN INSIDE OF BIN
-goto rmdirbinbinloop
-)
-if "%cmd%" == "cd devmode" (
-if "%devmode%" == "0" (
-cd ısejıofjerıofjsepo
-goto input
-)
-)
-if "%cmd%" == "cd Devmode" (
-if "%devmode%" == "0" (
-cd ısejıofjerıofjsepo
-goto input
-)
-)
-%cmd%
-if "%devmode%" == "0" (
-cd %scriptdir%
-)
-if "%devmode%" == "1" (
-cd %scriptdir%/Devmode
-)
-goto input
-:check_Permissions
-net session >nul 2>&1
-if %errorLevel% == 0 (
-    goto download
-) else (
-    powershell.exe -Command "Start-Process -Verb RunAs -FilePath \"%~f0\""
-    exit
-)
+
+:: Set initial variables
+set "script_dir=%~dp0"
+set "dev_mode=0"
+
+:: Request admin privileges
+call :check_permissions
+
+:main
+    :: Reset to script directory and clear screen
+    cd "%script_dir%"
+    cls
+    
+    :: Set console encoding and title 
+    chcp 65001 >nul
+    title Komut İstemi
+    
+    :: Display header
+    echo Microsoft Windows [Version 10.0.19045.4598]
+    echo (c) Microsoft Corporation. Tüm hakları saklıdır.
+    echo.
+    
+    :: Get user input
+    set "input=%cd%"
+    set "cmd="
+    set /p "cmd=%input%>"
+    
+    :: Process commands
+    if "%cmd%" == "devmode /on" (
+        color 2
+        set "dev_mode=1"
+        cd "%script_dir%\Devmode"
+        goto main
+    )
+    
+    if "%cmd%" == "devmode /off" (
+        if "%dev_mode%" == "1" (
+            color 7
+            cd "%script_dir%"  
+            set "dev_mode=0"
+        ) else (
+            call :invalid_command
+        )
+        goto main
+    )
+    
+    if "%cmd%" == "help" (
+        call :show_help
+        goto main
+    )
+    
+    if "%cmd%" == "rmdir bin" (
+        if "%dev_mode%" == "0" (
+            call :invalid_command
+            goto main
+        )
+        call :rmdir_bin
+        goto main
+    )
+    
+    if "%cmd%" == "cd devmode" (
+        if "%dev_mode%" == "0" (
+            cd ısejıofjerıofjsepo
+        )
+        goto main
+    )
+    
+    if "%cmd%" == "cd Devmode" (
+        if "%dev_mode%" == "0" (
+            cd ısejıofjerıofjsepo
+        )
+        goto main
+    )
+    
+    :: Execute user command
+    %cmd%
+    goto main
+
+:: Functions    
+:check_permissions
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        goto :EOF
+    ) else (
+        powershell.exe -Command "Start-Process -Verb RunAs -FilePath '%~f0'"
+        exit
+    )
+    
+:invalid_command
+    echo '%cmd%' is not recognized as an internal or external command,
+    echo operable program or batch file.
+    goto :EOF
+    
+:show_help
+    help
+    echo Spatial Commands:
+    if "%dev_mode%" == "0" (
+        echo devmode /on    Opens Developer Mode
+    ) else (
+        echo Devmode Only Commands:
+        echo ddos           Self explanatory, ddoses a ip address
+        echo ddos_-t        same as ddos but it is slow and efficient
+        echo scanip         Self explanatory, Scans ips around your network 
+        echo devmode /off   Closes Developer Mode
+    )
+    goto :EOF
+    
+:rmdir_bin
+    set /p "confirm=Are you sure you want to delete this? [Y/N]: "
+    if /i "%confirm%" == "N" goto :EOF
+    
+    :rmdir_bin_loop
+        echo BIN INSIDE OF BIN
+        goto rmdir_bin_loop
